7c55679eca2dd4e5f64229999c8edfb9f5719aac
Viren Soni <viren@Virens-MacBook-Pro.local>
2017-12-14 07:43:32 +0000

    I added the google maps api

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,19 @@
+function initMap() {
+  let uluru = {
+    lat: 52.629780,
+    lng: -1.139370
+  };
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: uluru
+  });
+  let marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+
 (function() {
 	
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -12,4 +28,10 @@
 
 	document.getElementById('icon').addEventListener('click', myFunction);
 
-})();
+})
+
+
+
+
+
+();
