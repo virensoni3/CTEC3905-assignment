51b3a6dc91dcf519502dbf7010a98edc7cc027cc
Viren Soni <viren@Virens-MacBook-Pro.local>
2017-12-14 15:38:33 +0000

    I added the responsive meta tag

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,7 +18,7 @@ function initMap() {
 	
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 	function myFunction() {
-	    var x = document.getElementById("myTopnav");
+	    let x = document.getElementById("myTopnav");
 	    if (x.className === "topnav") {
 	        x.className += " responsive";
 	    } else {
