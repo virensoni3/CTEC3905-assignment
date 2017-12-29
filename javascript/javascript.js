a85081d74a332833810459096c9ca251e40c1568
Viren Soni <viren@Virens-MacBook-Pro.local>
2017-12-29 18:23:52 +0000

    I commented the code and also indented it properly

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,7 @@
+/* This function below called initmap() has a variable called uluru. The latitude and longitude 
+are defined so that it pinpoints to the specific location, in this case Leicester. initmap() is in 
+global scope so that the callback for the google map api works.    */
+
 function initMap() {
   let uluru = {
     lat: 52.629780,
@@ -14,6 +18,9 @@ function initMap() {
 }
 
 
+/* Below is the function for the responsive hamburger menu. I have used this example from W3 schools.
+The link is: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp. It has been modified so that there is
+let instead of var */
 (function() {
 	
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -26,12 +33,7 @@ function initMap() {
 	    }
 	}
 
+    // There is an event listener so when a user clicks on the responsive menu, it runs myFunction.
 	document.getElementById('icon').addEventListener('click', myFunction);
 
-})
-
-
-
-
-
-();
+})();
